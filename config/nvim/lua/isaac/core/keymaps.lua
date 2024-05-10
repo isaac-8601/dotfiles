dbb4d81d3bbe3316bf1d3899f9a8f90633ecdca6
user <106764128+isaac-8601@users.noreply.github.com>
2024-05-10 15:19:05 -0700

    new keybinds, truezen add, wlogout add and testing

--- config/nvim/lua/isaac/core/keymaps.lua
+++ config/nvim/lua/isaac/core/keymaps.lua
@@ -71,11 +71,18 @@ map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
 -- Magic buffer-picking mode
 map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
 -- Sort automatically by...
-map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+map('n', '<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 
 -- Other:
 -- :BarbarEnable - enables barbar (enabled by default)
 -- :BarbarDisable - very bad command, should never be used
+
+-- TrueZen
+map("n", "<leader>zn", ":TZNarrow<CR>", {})
+map("v", "<leader>zn", ":'<,'>TZNarrow<CR>", {})
+map("n", "<leader>zf", ":TZFocus<CR>", {})
+map("n", "<leader>zm", ":TZMinimalist<CR>", {})
+map("n", "<leader>za", ":TZAtaraxis<CR>", {})
